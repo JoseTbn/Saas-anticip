b3c5ef4b308cb14ce8475e93a290df77c9584280
JoseTbn <josebatumeni@gmail.com>
2021-10-25 13:41:00 +0200

    first commit

---
+++ anticipteam/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import {global} from './globalStyles';
+import Headers from './Components/Header/Header';
+
+function App() {
+  return (
+    
+    
+    <Headers/>
+  );
+}
+
+export default App;
