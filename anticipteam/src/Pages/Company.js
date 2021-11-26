98562c3abd79c0ec03c96a8a5dafa1fffecb534f
JoseTbn <josebatumeni@gmail.com>
2021-11-26 17:36:11 +0100

    a lof of changes

--- anticipteam/src/Pages/Company.js
+++ anticipteam/src/Pages/Company.js
@@ -6,8 +6,9 @@ import Navbar from '../Components/Navbar/Navbar'
 import './Company/Company.css'
 import Forms from '../Components/Form/Forms';
 import Skill1 from '../assets/img/Skills 1.svg'
-
-
+import btnform from '../Components/buttons/btnform';
+import Button from '@mui/material/Button';
+   
 
 
 export default function Company() {
@@ -16,10 +17,20 @@ export default function Company() {
          {/* < GlobalStyle/> */}
           <Navbar/>
       <Forms/> 
+      
+           <div className="btn">
+         <Button   variant="contained"> 
+Next
+         </Button>
+         <Button   variant="contained"> 
+Previous
+         </Button>
+          </div>
       <img className='right__img' src={Skill1} alt="" />
           <WavyBackground/>   
-         
-         
+        
+
+  
       </> 
     ) 
 }
